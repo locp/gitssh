152785bfda96489515a7b10fd28ad0e173271725
dallinb <b.dalling@locp.co.uk>
2015-12-04 13:41:28 +0000

    Added a bit more framework to the module.

--- spec/classes/gitssh_spec.rb
+++ spec/classes/gitssh_spec.rb
@@ -2,6 +2,9 @@ require 'spec_helper'
 describe 'gitssh' do
 
   context 'with defaults for all parameters' do
-    it { should contain_class('gitssh') }
+    it {
+      should contain_class('gitssh')
+      should contain_package('git')
+    }
   end
 end
