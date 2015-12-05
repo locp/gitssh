b4f31e872f651faee6d7627f7cf23639c1dfdd3b
dallinb <b.dalling@locp.co.uk>
2015-12-05 18:02:50 +0000

    Added Coveralls.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 require 'rspec-puppet'
+require 'coveralls'
 
 fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
 
@@ -7,3 +8,6 @@ RSpec.configure do |c|
   c.manifest_dir = File.join(fixture_path, 'manifests')
   c.environmentpath = File.join(Dir.pwd, 'spec')
 end
+
+Coveralls.wear!
+at_exit { RSpec::Puppet::Coverage.report! }
